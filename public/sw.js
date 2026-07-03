5e3355b43160bac6eabd0eed3fd023cbfff629d9
denicerobertson72-source <denicerobertson72@gmail.com>
2026-07-03 09:46:22 -0400

    Refresh PWA cache version

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "qcrc-pwa-v2";
+const CACHE_VERSION = "qcrc-pwa-v3";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const ASSET_CACHE = `${CACHE_VERSION}-assets`;
 const PAGE_CACHE = `${CACHE_VERSION}-pages`;
